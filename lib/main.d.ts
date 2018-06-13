4fd2908e4199ad72efb62f1ac8fd1a8349a3ee1d
William Skidmore <wskidmore@gmail.com>
2018-06-12 22:06:23 -0600

    skip making bundle, use newer module folder with fancy exports

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,11 +1,5 @@
-import { Client as _Client, ConnectionStatus as _ConnectionStatus, MessageSendError as _MessageSendError, MessageSendErrorCause as _MessageSendErrorCause } from './client';
-export declare var Client: typeof _Client;
-export declare var ConnectionStatus: typeof _ConnectionStatus;
-export declare var MessageSendError: typeof _MessageSendError;
-export declare var MessageSendErrorCause: typeof _MessageSendErrorCause;
-import { resume as _resume } from './extensions/resume';
-export declare var resume: typeof _resume;
-import { presenceBatch as _presenceBatch } from './extensions/presence-batch';
-export declare var presenceBatch: typeof _presenceBatch;
-import { voiceBatch as _voiceBatch } from './extensions/voice-batch';
-export declare var voiceBatch: typeof _voiceBatch;
+export * from './client';
+export * from './interfaces';
+export * from './extensions/presence-batch';
+export * from './extensions/resume';
+export * from './extensions/voice-batch';
